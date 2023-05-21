3e8a80783003fdedc156abe4c229ffa87c472604
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-05-20 23:12:56 -0400

    adding a back button. and creating the questions that will display.

--- game.js
+++ game.js
@@ -7,82 +7,96 @@ const progressBarFull = document.querySelector('#progressBarFull');
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
-let qustionCounter= 0
+let questionCounter = 0
 let availableQuestions = []
 
-let quetoins = [
+let questions = [
     {
-        question: 'what is 2+2',
-        choice1:'2',
-        choice2:'4',
-        choice3: '21',
-        choice4: '17',
+        question: 'Which of the following is NOT a valid Java identifier?',
+        choice1: 'myVariable',
+        choice2: '123variable',
+        choice3: '_variable',
+        choice4: 'Variable123',
         answer: 2,
-
     },
     {
-        question: 'what is 2+2',
-        choice1:'2',
-        choice2:'4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-
+        question: 'Which of the following is the correct way to declare and initialize a constant variable in Java?',
+        choice1: 'constant int x = 5;',
+        choice2: 'final x = 5;',
+        choice3: 'final int x = 5;',
+        choice4: 'int x = final 5;',
+        answer: 3,
     },
     {
-        question: 'what is 2+2',
-        choice1:'2',
-        choice2:'4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-
+        question: 'What is the purpose of the break statement in Java?',
+        choice1: 'It terminates the current loop or switch statement.',
+        choice2: 'It skips the current iteration of a loop.',
+        choice3: 'It transfers the program control to a specified label.',
+        choice4: 'It throws an exception to be caught by a try-catch block.',
+        answer: 1,
     },
     {
-        question: 'what is 2+2',
-        choice1:'2',
-        choice2:'4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-
+        question: 'Which HTML tag is used to define a hyperlink?',
+        choice1: '<a>',
+        choice2: '<h1>',
+        choice3: '<p>',
+        choice4: '<div>',
+        answer: 1,
     },
-]
+];
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
 
-startGame = () =>{
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
-    
-}
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
 
-getNewQuestion = () =>{
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
-        localStorage.setItem('mostRecentScore',score)
-        return window.location.assign('/end.html')
+const getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+        return window.location.assign('/end.html');
     }
 
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-questioncounter++
-progressText.innerText =`Question ${questionCounter} of ${Max_Quetsions}`
-progressBarFull.getElementsByClassName.width= `${(questioncounter/MAX_QUESTIONS) * 100}%`
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
 
-const questionsIndex= Math.florr(Math.ramdon() * availableQuestions.length)
-currentQuestion = availableQuestions{questionsIndex}
-question.innerText= currentQuestion.question
+    choices.forEach((choice, index) => {
+        choice.innerText = currentQuestion['choice' + (index + 1)];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+
+    acceptingAnswers = true;
+};
 
 choices.forEach(choice => {
-    const number = cohice.dataset['number']
-    choice.innerText = currentQuestion['chhoice' + number]
+    choice.addEventListener('click', e => {
+        if (!acceptingAnswers) return;
 
-})
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
 
-availableQuestions.splice(questionIndex, 1)
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
+        if (classToApply === 'correct') {
+            incrementScore(SCORE_POINTS);
+        }
 
+        selectedChoice.parentElement.classList.add(classToApply);
 
-}
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
